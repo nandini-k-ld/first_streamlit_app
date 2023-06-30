1f93ea07b8389c9166faa055a8048b3a146ad388
nandini-k-ld <121931809+nandini-k-ld@users.noreply.github.com>
2023-06-30 15:04:55 +0530

    Update streamlit_app.py
    added header and 3 lines of txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('Hi, How are you')
+streamlit.title('My parents new healthy dinner')
+
+streamlit.header('Breakfast menu')
+streamlit.txt ('Omega 3 & Blueberry Oatmeal')
+streamlit.txt('kale, Spinach & Rocket Smoothie')
+streamlit.txt('Hard-Boiled Free-Range Egg')
